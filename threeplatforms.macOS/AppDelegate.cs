4416333fb08d93ee4851da30b34356a27874ad1d
Paul Johnson <paul@snuberry-software.co.uk>
2019-02-03 23:58:38 +1000

    Initial import

---
+++ threeplatforms.macOS/AppDelegate.cs
@@ -0,0 +1,38 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace threeplatforms.macOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow _window;
+
+        public override NSWindow MainWindow => _window;
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 200, 800, 600);
+            _window = new NSWindow(rect, style, NSBackingStore.Buffered, false)
+            {
+                Title = "Three platforms"
+            };
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
